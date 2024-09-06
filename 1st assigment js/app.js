74d7a78622df3c8503a1a122e8135c3e160978fc
SufyanMamji54 <sufyanmamji54@gmail.com>
2024-09-06 16:42:54 +0500

    Add files via upload

---
+++ 1st assigment js/app.js
@@ -0,0 +1,146 @@
+// Chapter no 1
+
+// Q : no 1
+alert("Error! Please enter a valid password");
+// // Q : no 2
+alert("Welcome to js Land... \n Happy coding!");
+// // Q : no 3
+alert("Welcome to js Land...");
+// // Q : no 4
+alert("Happy coding");
+// // Q : no 5
+alert("hello... I can run js through my web browser console");
+
+
+
+// Chapter no 2
+
+// Q : no 4
+
+var username = "jhone doe"
+alert(username)
+
+var username = "15 year old"
+alert(username)
+
+var username = "Certified mobile application devlopment"
+alert(username)
+
+// Q : no 5
+var username = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(username)
+
+// Q : no 6
+var useremail = "My email adress example@example.com"
+alert(useremail)
+
+// Q : no 7
+var userpara = "I am trying to learn from the book a smarter \n way to learn javascript"
+alert(userpara)
+
+// Q : no 8
+document.write("Yah! I can write HTML content through javascript")
+
+// Q : no 9
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
+
+// Chapter no 3
+
+// Q : no 1
+var userage = "i am 15 year old"
+alert(userage)
+
+// Q : no 2
+var userpara = "you have visited the 15 times"
+alert(userpara)
+
+// Q : no 3
+document.write("My birth is year 1990 <br> data type of my declare variable is number")
+
+// Q : no 4
+document.write("jhon doe orderd t.shirt (s) on xyz clothing store")
+document.write("<br>")
+
+
+// Chapter No :04
+
+// Q : 01
+var firstVariable = ""
+var _2ndVariable = ""
+var $3rd_variable = ""
+
+
+// Q :02
+var firstVariable = "Legal"
+var _2ndVariable = "Legal"
+var $3rd_variable = "Legal"
+var $fourth$Variable = "Legal"
+var fifth123 = "Legal"
+
+// var 1stVariable = "Illegal"
+// var +secondVariable = "Illegal" 
+// var third variable = "Illegal"
+// var fourth $variable = "Illegal"
+// var 5$th_Variable = "Illegal"
+
+// Q : 03 
+document.write("a)" + "  " +"Rules for naming JS variables") 
+document.write("<br>")
+
+document.write('b)')
+document.write("Variable names can only contain" + " "+ "Numbers" + " "+ "$" + " "+ "Letters" + " "+ "_")
+document.write("<br>")
+
+document.write('c)')
+document.write("Variables must begin with a"+ " "+ "Letter" + " "+ "$"+ "or"+ " "+ "_." +" " + "For example $name, _name or name")
+
+document.write("<br>")
+document.write('c)')
+document.write("Variable names are case Sensitivex")
+
+document.write("<br>")
+document.write('d)')
+document.write("Variable names should not be JS Keyword")
+
+
+
+
+alert ("Youre learning JavaScript!");
+
+
+// Chapter 2 (Variables for string)
+
+var camelCase =""
+
+var anyVariable = "" 
+ anyVariable = "Sufyan"
+
+ var teamName = "Pakistan"
+ alert(teamName)
+
+ var bestMan = "Charlie"
+ bestMan = "Charlie Updated"
+ console.log(bestMan)
+
+
+//  Chapter 3 (Variables for numbers)
+
+var caseQty = 0
+
+var assignCaseQty;
+assignCaseQty = 144
+
+var num = 9 
+
+var sum ;
+sum = 10+20
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal+shippingCharge
+
+var addNumber = 10
+addNumber = addNumber+5
+
+
